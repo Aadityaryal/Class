7ff4b2aa22e1e2b47e5e338d9a21736bccea681e
Aaditya <aadityaryal8@gmail.com>
2024-04-21 12:59:49 +0545

    Week1 Task 4 added

---
+++ Week1/JavaArgument.java
@@ -0,0 +1,13 @@
+package Week1;
+//Task 4
+public class JavaArgument {
+    public static void main(String[] args) {
+        if(args.length<2){
+            System.out.println("Not enough args");
+            return;
+        }
+        System.out.println("First arg is "+ args[0]);
+        System.out.println("Second arg is "+ args[1]);
+
+    }
+}
